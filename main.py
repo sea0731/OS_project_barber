4a659ffa511db190112f30ece2119b4fcbe588ea
ssr <sea0731qwetyuiop@gmail.com>
2017-11-23 23:42:11 +0800

    main.py first version, not complete

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
-import threading
+from threading import Thread
 import time
+import numpy as np
+import Queue
 import barber
 import client
-import numpy as np
+
+class endStore:
+    def __init__():
+        self.end = "N"
+
+def end():
+    while end != Y:
+        endStore.end = input("Do you want to close the store?(Y or N)")
+
+        if(end != Y || end != N)
+            print("please type Y or N")
 
 def main:
     global mBarber = input("input barber: ")
+    global nChair = input("input the number of chire: ")
     global Clientp = input("input cient: ")
     
-    
-    for i in range(1, mBarber)
-        thread = barber.Barber("barber" + str(i))
-        BarberThreads
+    threadID1 = 1
+    threadID2 = 1
+    BarberThreads = []
+    ClientQueue = Queue.Queue(nChair)
 
+    for threadID1 in range(1, mBarber, ClientQueue):
+        thread = barber.Barber(threadID1, "barber" + str(threadID1))
+        thread.start()
+        BarberThreads.append(thread)
 
-    while True:
+    while gatattr(endStore, "end") != "Y":
         sleep(np.random.poisson(Clientp, 1))
-        
+        thread = client.Client(threadID2, "client"+str(threadID2))
+        thread.start()
+        ClientQueue.put(thread)
+        threadID2++
 
+    ClientQueue.join()
