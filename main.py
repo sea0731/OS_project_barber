862ce97fc34d478a6011216f29b91fa5a58f436e
ssr <sea0731qwetyuiop@gmail.com>
2017-11-23 20:26:55 +0800

    poisson random add in main

---
+++ main.py
@@ -0,0 +1,20 @@
+import threading
+import time
+import barber
+import client
+import numpy as np
+
+def main:
+    global mBarber = input("input barber: ")
+    global Clientp = input("input cient: ")
+    
+    
+    for i in range(1, mBarber)
+        thread = barber.Barber("barber" + str(i))
+        BarberThreads
+
+
+    while True:
+        sleep(np.random.poisson(Clientp, 1))
+        
+
