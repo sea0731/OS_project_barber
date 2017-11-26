d61ab9b1064a96ecc2bf4472d0b0bb9bd58fb724
ssr <sea0731qwetyuiop@gmail.com>
2017-11-26 18:43:34 +0800

    add commit

--- main.py
+++ main.py
@@ -5,46 +5,50 @@ import queue
 import barber
 import client
 
-global onChair 
+global onChair          #onChair is record how many clients in waitting room
 onChair = 0
 
-class endStore:
+class endStore:         #endStore contain value wheather we end the program
     def __init__():
         self.end = "N"
 
-def end():
-    while end != Y:
+def end():              #end function try to get the input if user want to terminate the program or not
+    while end != "Y":
         endStore.end = input("Do you want to close the store?(Y or N)")
 
-        if end != Y or end != N :
+        if end != "Y" or end != "N" :
             print("please type Y or N")
 
-def main():
-    global mBarber 
+def main():             #main function of  barber-client problem
+    global mBarber      #mBarber record "m" barber is working
     mBarber = input("input barber: ")
-    global nChair 
+    global nChair       #nChair record "n" chair in waiting room
     nChair = input("input the number of chire: ")
-    global Clientp
+    global Clientp      #Clientp record the frequent parameter of clients come
     Clientp = input("input cient: ")
     
-    cond = threading.Condition()
-    enter = threading.Condition()
-    
-    threadID1 = 1
-    threadID2 = 1
-    BarberThreads = []
-    ClientQueue = Queue.Queue(nChair)
+    wakeUpbarber = threading.Condition()    #Wheather the client can wake up barbers  
+    enter = threading.Condition()           #Wheather the client can enter the waiting room
 
-    for threadID1 in range(1, mBarber, ClientQueue):
+    callClient = threading.Condition()      #Wheather the barber can call next client
+    
+    threadID1 = 1                           #threadID1 record the number of barbers
+    threadID2 = 1                           #threadID2 record the number of clients
+    BarberThreads = []                      #BarberThreads cantain all barbers who are working
+    ClientQueue = Queue.Queue(nChair)       #ClientQueue contain all clients in the store
+
+    for threadID1 in range(1, mBarber):     #Create thread of barbers
         thread = barber.Barber(threadID1, "barber" + str(threadID1))
         thread.start()
         BarberThreads.append(thread)
 
-    while gatattr(endStore, "end") != "Y":
-        sleep(np.random.poisson(Clientp, 1))
-        thread = client.Client(threadID2, "client"+str(threadID2), cond, enter)
+    while gatattr(endStore, "end") != "Y":  #If user input "Y" terminate creating client
+        sleep(np.random.poisson(Clientp, 1))    #randomly create client
+        thread = client.Client(threadID2, "client"+str(threadID2), wakeUpbarber, enter)
         thread.start()
         ClientQueue.put(thread)
-        threadID2 += threadID2
+        threadID2 += 1
 
     ClientQueue.join()
+
+main()
