f2ce0595d19fdfb388300a82b16c70327daf122b
NCCU104703012 <32267075+NCCU104703012@users.noreply.github.com>
2017-11-26 20:34:24 +0800

    client fixd 171126

--- client.py
+++ client.py
@@ -1,5 +1,4 @@
 import  threading
-import  barber
 
 global mBarber
 global nChair
@@ -8,24 +7,29 @@ global Clientp
 global onChair
 
 class Client( threading.Thread ) :
-    def __init__( self, threadID, clientName, cond, enter ):
+    def __init__( self, threadID, clientName, condbar, cond, enter ):
         threading.Thread.__init__( self )
         self.threadID = threadID
         self.clientName = clientName
         self.cond = cond 
+        self.condbar = condbar
         self.enter = enter #main宣告兩個鎖cond , enter傳入
 
 
     def run( self ):
+
         self.enter.acquire()
 
         if onChair < nChair :
+            onChair += 1
             self.enter.release()
 
             #enter shop
-            onChair += 1
+            
             self.cond.acquire()
-            barber.Barber.cond.notify(1) #barber中的鎖先給個名字cond
+            self.condbar.acquire()
+            self.condbar.notify() 
+            self.condbar.release()
             self.cond.wait()
             self.cond.release()
             print("{0} release cond".format( self.clientName ))
@@ -33,3 +37,4 @@ class Client( threading.Thread ) :
         else :
             print("{0} leave shop".format( self.clientName ))
             self.enter.release()
+
