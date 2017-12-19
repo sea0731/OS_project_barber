efc7992b2709b01a0dce47affaa8c733e3ed390f
goodpdt <goodpdt@yahoo.com.tw>
2017-12-19 21:12:58 +0800

    Add gui main.py

--- main.py
+++ main.py
@@ -5,18 +5,19 @@ import numpy as np
 import Queue
 import Tkinter as tk
 import sys
+from Tkinter import END
 
 #win = tk.Tk()
 #win.title("The Sleeping Barber Problem")
 #win.geometry('500x500')
 #tk.Label(win, text="Welcome to the Barber Shop!", pady=20).pack()
 
-global mBarber
-mBarber=0
-global nChair
-nChair=0
-global Clientp
-Clientp=0
+#global mBarber
+mBarber=100
+#global nChair
+nChair=100
+#global Clientp
+Clientp=100
 
 
 class popupWindow(object):
@@ -42,27 +43,34 @@ class popupWindow(object):
         self.b.grid(row=3, column=0, pady=4)
     def cleanup(self):
         #self.value=self.e1.get()
-	mBarber = self.e1.get()
+        global mBarber
+        global nChair
+        global Clientp
 	
+        mBarber = self.e1.get()
 	nChair = self.e2.get()
 	Clientp = self.e3.get()
-	print mBarber
-        print nChair
-        print Clientp
+	#print mBarber
+        #print nChair
+        #print Clientp
         self.top.destroy()
 
         main()
 
 class mainWindow(object):
     def __init__(self,master):
-	
 	self.master=master
         self.b=tk.Button(master,text="Enter Value!",command=self.popup)
+        self.b.place(x=500, y=0)
         self.b.pack()
         #self.b2=tk.Button(master,text="Start",command=lambda: sys.stdout.write(self.entryValue()+'\n'))
 	#self.b2=tk.Button(master, text="Start", command=main())
-	self.b2=tk.Button(master, text="Start", command=quit)
+	self.b2=tk.Button(master, text="END", command=quit)
         self.b2.pack()
+        self.cIn=tk.Text(self.master, height=1, width=30)
+        self.cIn.pack()
+        self.cOut=tk.Text(self.master, height=1, width=30)
+        self.cOut.pack()
     def popup(self):
         self.w=popupWindow(self.master)
         self.b["state"] = "disabled" 
@@ -70,10 +78,47 @@ class mainWindow(object):
         self.b["state"] = "normal"
     def entryValue(self):
         return self.w.value
+    def printClientIn(self):
+        global clientIn
+        self.cIn.delete(0.0, END)
+        self.cIn.insert(END, clientIn)
+        #self.cIn.update_idletasks()
+        self.cIn.config(fg='black')
+        #self.T.config(state='disabled')
+        self.cIn.pack()
+        self.cIn.update_idletasks()
+        print "PC", clientIn
+        #p1 = tk.PhotoImage(file="p1.png")
+        #img = tk.Label(image=p1)
+        #img.image = p1
+        #img.pack()
+        #img.place(x=0, y=0)
+        #img.update_idletasks()
+    def printClientOut(self):
+        global clientOut
+        self.cOut.delete(0.0, END)
+        self.cOut.insert(END, clientOut)
+        #self.cOut.update_idletasks()
+        self.cOut.config(fg='black')
+        self.cOut.pack()
+        self.cOut.update_idletasks()
+        print "PC", clientOut
+        #img.image.blank()
+        #img.image = None
 
-#tk.Button(win, text='Begin', command=begin).pack()
-
-#win.mainloop()
+'''
+class newWindow(object):
+    def __init__(self,master):
+        self.master = master
+        self.T=tk.Text(self.master, height=1, width=30)
+        self.T.pack()
+    def printT(self):
+        global temp
+        self.T.delete(0.0, END)
+        self.T.insert(END, temp)
+        self.T.pack()
+        print "new", temp
+'''
 
 global onChair          #onChair is record how many clients in waitting room
 onChair = 0
@@ -90,7 +135,7 @@ class end( threading.Thread ):              #end function try to get the input i
         global endS
 
         while endS.end != "Y":
-            endS.end = input("Do you want to close the store?(Y or N)")
+            endS.end = input("Do you want to close the store?(Y or N)\n")
 
             if endS.end != "Y" or end != "N" :
                 print("please type Y or N")
@@ -112,6 +157,7 @@ class Barber( threading.Thread ):
         global ClientQueue
 
         global endS
+        global clientOut
 
         while getattr(endS, "end") != "Y":
             self.callB.acquire()
@@ -132,6 +178,10 @@ class Barber( threading.Thread ):
             time.sleep(3)
 
             print (C.clientName, " out")
+            print "here1"
+            temp1=str(C.clientName)+" out"
+            clientOut=temp1
+            m.printClientOut()
 
             self.enter.acquire()
             while ClientQueue.empty() != True:
@@ -144,6 +194,10 @@ class Barber( threading.Thread ):
                 time.sleep(3)
 
                 print (C.clientName, " out")
+                print "here2"
+                temp2=str(C.clientName)+" out"
+                clientOut=temp2
+                m.printClientOut()
 
                 self.enter.acquire()
 
@@ -182,11 +236,14 @@ class Client( threading.Thread ) :
 def main():             #main function of  barber-client problem
     global mBarber      #mBarber record "m" barber is working
     #mBarber = input("input barber: ")
-    #mBarber = int(mBarber)
+    mBarber = int(mBarber)
     global nChair       #nChair record "n" chair in waiting room
     #nChair = input("input the number of chair: ")
     global Clientp      #Clientp record the frequent parameter of clients come
     #Clientp = input("input client paramater: ")
+    #print mBarber
+    #print nChair
+    #print Clientp
 
     wakeUpbarber = threading.Condition()    #Wheather the client can wake up barbers  
     enter = threading.Condition()           #Wheather the client can enter the waiting room
@@ -213,7 +270,22 @@ def main():             #main function of  barber-client problem
 
         thread = Client(threadID2, "client"+str(threadID2), wakeUpbarber, callClient, enter)
 
-        print "client in", threadID2
+        #print "client in", threadID2
+        global clientIn
+        temp="client"+ str(threadID2)+" in"
+        clientIn = temp
+        
+        #a=mainWindow(win)
+        #a.printClient()
+
+        m.printClientIn()
+
+        '''
+        win2 = tk.Tk()
+        b=newWindow(win2)
+        b.printT()
+        win2.mainloop()
+        '''
 
         q_Lock.acquire()
         if ClientQueue.full() != True:
@@ -234,13 +306,13 @@ def main():             #main function of  barber-client problem
 if __name__ == "__main__":
     win=tk.Tk()
     win.title("The Sleeping Barber Problem")
-    C = tk.Canvas(win, bg="white", height=500, width=500)
+    D = tk.Canvas(win, bg="white", height=500, width=500)
     background_image = tk.PhotoImage(file="2.png")
     background_label = tk.Label(win, image=background_image)
     background_label.place(x=0, y=0, relwidth=1, relheight=1)
     
     tk.Label(win, text="Welcome to the Barber Shop!", pady=20)
-    C.pack()
+    D.pack()
     m=mainWindow(win)   
  
     #main()
